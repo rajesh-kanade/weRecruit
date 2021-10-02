032c67c7f59647efc531b6cac7d5f093bd8b271b
raka <43023475+rkanade@users.noreply.github.com>
2021-10-03 00:16:44 +0530

    first cut for werecruit

---
+++ src/werecruit/webApp.py
@@ -0,0 +1,304 @@
+from flask import (
+    Flask,
+    flash,
+    g,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for
+)
+
+import logging
+import userUtils
+
+
+class User:
+    
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        
+
+    #def __repr__(self):
+    #    return f'<User: {self.username}>'
+
+users = []
+
+'''
+users.append(User(id=1, username='Anthony', password='password'))
+users.append(User(id=2, username='Becca', password='secret'))
+users.append(User(id=3, username='Carlos', password='somethingsimple'))
+'''
+
+app = Flask(__name__)
+app.secret_key = 'somesecretkeythatonlyishouldknow'
+
+logging.basicConfig(level=logging.DEBUG)
+'''
+@app.before_request
+def before_request():
+    g.user = None
+
+    if 'user_id' in session:
+        user = [x for x in users if x.id == session['user_id']][0]
+        g.user = user
+'''
+@app.route('/')
+def home():
+    return render_template('index.html')        
+        
+'''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user_id', None)
+
+        username = request.form['username']
+        password = request.form['password']
+        
+        user = [x for x in users if x.username == username][0]
+        if user and user.password == password:
+            session['user_id'] = user.id
+            return redirect(url_for('profile'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
+
+@app.route('/profile')
+def profile():
+    if not g.user:
+        return redirect(url_for('login'))
+
+    return render_template('profile.html')
+'''
+@app.route('/user/showSignupPage')
+def show_signup_page():
+
+    return render_template('sign_up.html')
+
+@app.route('/user/createUser',  methods = ['POST'])
+def create_user():
+    
+    #request.form['id'] = email
+    userAttrs = request.form.to_dict()
+    
+    userAttrs['id'] = request.form['email'] 
+    userAttrs['status'] =userUtils.Status.active.value
+
+    results = userUtils.create_user( userAttrs)
+
+    if (results[0] == userUtils.RetCodes.success.value):        
+        flash (results[1])
+        return render_template('index.html')
+    else:
+        #return jsonify({RET_CODE: results[0],RET_MSG: results[1]}),400
+        #error = results[1]
+        flash (results[1],"error")
+        return render_template('sign_up.html')
+
+
+    
+def render_Sign_in():
+
+    return render_template('Sign_in.html')
+
+def render_otp():
+
+    return render_template('otp.html')        
+
+def sign_up():
+
+    app.logger.info("hello")
+
+    render_sign_up()
+
+    name = ''
+    
+    email = ''
+     
+
+    if request.method == 'POST':
+
+        name = request.form['name']
+        email = request.form['email']
+        
+        app.logger.info(name)
+        app.logger.info(type(name))
+        app.logger.info(email)
+        app.logger.info(type(email))
+        
+    
+    if(name!="" and email!=""):
+
+        (retCode,msg) = userUtils.signUp(name, email,None)
+        #users.append(User(name=name, surname=surname, email=email)
+        #flash("Sign up successful")  
+        #app.logger.info(users[-1].name)
+        #app.logger.info(users[-1].surname)
+        #app.logger.info(users[-1].email)
+         
+        return render_template('home.html')
+    
+    else:
+
+        name = ""
+        email = ""
+
+        flash("All fields are to be filled")    
+        return render_template('Sign_up.html')
+
+    
+
+@app.route('/Sign in', methods = ['GET', 'POST'])
+def sign_in():
+
+    app.logger.info("hello_1")
+    print("hello_1")
+
+    render_Sign_in()
+
+    print("rendered sign in")
+    
+    email = ''
+    otp = ''
+
+    print("variable initialization done")
+
+    if request.method == 'POST':
+
+        email = request.form['email']
+        otp = request.form['otp']
+        app.logger.info(email)
+        app.logger.info(type(email))
+        app.logger.info(otp)
+        app.logger.info(type(otp))
+        
+    
+    if(otp!="" and email!=""):
+        
+        ans = userUtils.userSignIn(email, otp)
+        print(ans)
+        if(ans == 0):
+            print("Login Successful")
+            #flash("Login Successful")
+            return render_template('home.html')
+
+        elif(ans == -1):
+            print("Login Failed")
+            flash("Login Failed")
+            return render_template('Sign_in.html')
+
+        elif(ans == -2):
+            flash("Login failed with exception")
+            print("Login failed with exception")
+            return render_template('Sign_in.html')
+
+        #users.append(User(name=name, surname=surname, email=email)
+        #flash("Sign up successful")  
+        #app.logger.info(users[-1].name)
+        #app.logger.info(users[-1].surname)
+        #app.logger.info(users[-1].email)
+         
+        return render_template('home.html')
+    
+    else:
+
+        #return callotpGenMethod(email)
+
+        #otp = ""
+        #email = ""
+ 
+        flash("All fields are to be filled")    
+        return render_template('Sign_in.html')
+    
+@app.route('/otpGeneration', methods = ['POST']) 
+def callotpGenMethod():
+
+    #app.logger.info("generating otp...")
+
+    print("otp gen")
+
+    #render_otp()
+
+    #print(flag)
+
+    email = ''
+
+    print("heyyy")
+
+    if request.method == 'POST':
+
+        email = request.form['email']
+        
+        app.logger.info(email)
+        print(email)
+        app.logger.info(type(email))
+        print(type(email))
+        
+    
+    if(email!=""):
+
+        (ans,msg) = userUtils.getOTP(email)
+        print(ans)
+        app.logger.info(ans)
+
+        if(ans == 0):
+
+            #global flag
+
+            print("returning to sign in page")
+            
+            #print(flag)
+            #flash("Once you get the otp proceed further")
+            #render_template('Sign_up.html', messages=['email':''])
+            return redirect('/Sign in')
+            #return render_template('otp.html')
+
+        elif(ans == -1):
+
+            flash("Email address provided is not registered with us.")
+
+            return render_template('otp.html')       
+    
+
+        elif(ans == -2):
+
+            print("in else flag is " + str(flag))
+            
+            print("else")
+            
+            flash("otp generation failed. Please try again.")
+            
+            #flash("Once you get the otp proceed further")
+                 
+            return render_template('otp.html')       
+
+    else:
+
+        #global flag
+
+        #email = ""
+
+        print("flag in final else " + str(flag))
+
+        print("else")
+
+        flash("Email cannot be blank.")
+        
+        return render_template('otp.html')    
+
+
+@app.route('/showotpGenerationpage', methods = ['GET']) 
+def callshowotpGenerationpage():
+
+    print(" show otp gen")
+
+    return render_otp()
+
+    #render_template('otp.html')        
+
+
+if __name__ == "__main__":
+    app.run()
