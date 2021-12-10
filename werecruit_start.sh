bc63620708578e915d650bd04b9c09b0d1be0a0c
raka <43023475+rkanade@users.noreply.github.com>
2021-12-11 00:14:52 +0530

    few fixes

--- werecruit_start.sh
+++ werecruit_start.sh
@@ -6,7 +6,8 @@ nohup python3 ./src/werecruit/webApp.py & #wr_webapp
 echo "Starting scheduler"
 nohup python3 ./src/werecruit/cronjobs.py & #wr_sched
 
+echo 
 echo "weRecruit started."
 
-echo "Press any key to return to command prompt !!!!"
+
 
