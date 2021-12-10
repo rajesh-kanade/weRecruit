5618c6691428c30ff4c60332125b1328b9cab2e5
raka <43023475+rkanade@users.noreply.github.com>
2021-12-11 00:07:21 +0530

    start script

--- werecruit_start.sh
+++ werecruit_start.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 echo "Starting web app"
-nohup python3 ./src/werecruit/webApp.py &
+nohup python3 ./src/werecruit/webApp.py & #wr_webapp
 
 echo "Starting scheduler"
-nohup python3 ./src/werecruit/cronjobs.py &
+nohup python3 ./src/werecruit/cronjobs.py & #wr_sched
 
-echo "weRecruit started !!!"
+echo "weRecruit started."
+
+echo "Press any key to return to command prompt !!!!"
 
