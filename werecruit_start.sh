343bed627722ccb790f815bcdc69193dc2742401
raka <43023475+rkanade@users.noreply.github.com>
2021-12-19 13:33:40 +0530

    gunicorn integration

--- werecruit_start.sh
+++ werecruit_start.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-echo "Starting web app"
-nohup python3 ./src/werecruit/webApp.py & #wr_webapp
+echo "Starting weRecruit web app"
+gunicorn --pythonpath /etc/werecruit/src/werecruit --daemon --workers 4 --bind 0.0.0.0:5000 webApp:app
 
-echo "Starting scheduler"
+echo "Starting weRecruit scheduler"
 nohup python3 ./src/werecruit/cronjobs.py & #wr_sched
 
 echo 
-echo "weRecruit started."
+echo "weRecruit all processes started."
 
 
 
