b2db8c3707087f5b2b05eba15543f8d59812ec8b
raka <43023475+rkanade@users.noreply.github.com>
2022-04-12 00:02:03 +0530

    upgrade statments resequenced for easy upgrade

--- conf/upgrade_db.sql
+++ conf/upgrade_db.sql
@@ -1,5 +1,35 @@
 
-/* ******** successfully upgrade prod on 28-March-2022 on contabo2 server ******************
+/* start upgrade prod on 29-Dec-2021 */
+ 
+ALTER TABLE public.wr_resumes
+    ADD COLUMN json_resume jsonb ;
+/* end upgrade prod on 29-Dec-2021 */
+
+
+/* start upgrade prod on 12-Feb-2022 */
+ALTER TABLE public.wr_jds
+    ADD COLUMN client_jd bytea ;
+
+ALTER TABLE public.wr_resumes
+    ADD COLUMN resume_content bytea ;
+
+ALTER TABLE public.wr_resumes ADD COLUMN ts tsvector 
+    GENERATED ALWAYS AS (to_tsvector('english', json_resume)) STORED;
+
+CREATE INDEX ts_idx ON wr_resumes USING GIN (ts);
+/* end upgrade prod on 12-Feb-2022 */
+
+
+/* ******** start upgrade prod on 14-March-2022 ***************** */
+ALTER TABLE public.wr_jds
+  RENAME COLUMN yrs_of_exp TO min_yrs_of_exp;
+
+ALTER TABLE public.wr_jds
+    ADD COLUMN max_yrs_of_exp NUMERIC(4,2) ;
+/* ******** start upgrade prod on 14-March-2022 ***************** */
+
+
+/* ******** start upgrade prod on 28-March-2022 on contabo2 server *******************/
 
 ALTER TABLE public.wr_resumes
     ADD COLUMN is_deleted boolean NOT NULL DEFAULT false ;
@@ -13,35 +43,4 @@ insert into public.application_status_codes(id,description,is_deleted) values (2
 insert into public.application_status_codes(id,description,is_deleted) values (3,'Initial screening failed',false);
 
 
- **********************************  */
-
-
-
-/* ******** successfully upgrade prod on 14-March-2022 *****************
-ALTER TABLE public.wr_jds
-  RENAME COLUMN yrs_of_exp TO min_yrs_of_exp;
-
-ALTER TABLE public.wr_jds
-    ADD COLUMN max_yrs_of_exp NUMERIC(4,2) ;
- *********************************  */
-
-
-/* successfully upgrade prod on 12-Feb-2022 
-ALTER TABLE public.wr_jds
-    ADD COLUMN client_jd bytea ;
-
-ALTER TABLE public.wr_resumes
-    ADD COLUMN resume_content bytea ;
-
-ALTER TABLE public.wr_resumes ADD COLUMN ts tsvector 
-    GENERATED ALWAYS AS (to_tsvector('english', json_resume)) STORED;
-
-CREATE INDEX ts_idx ON wr_resumes USING GIN (ts);
-*/
-
- 
-/* successfully upgrade prod on 29-Dec-2021 
- 
-ALTER TABLE public.wr_resumes
-    ADD COLUMN json_resume jsonb ;
-*/
+/* ******** end upgrade prod on 28-March-2022 on contabo2 server *******************/
