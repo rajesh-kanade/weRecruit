47f16c7eab60d20132b1191ae7d645476c626933
11Manav <manavjb31@gmail.com>
2022-05-18 13:29:03 +0530

    #65 all changes

--- src/werecruit/webForms.py
+++ src/werecruit/webForms.py
@@ -74,8 +74,13 @@ class JDForm(FlaskForm):
 
     location = StringField('Location')
 
-    min_yrs_of_exp = DecimalField("Minimum years of Experience",validators=[DataRequired(NumberRange(min=0,max=99,message="Min experience needs be in range of 0 to 99"))])
-    max_yrs_of_exp = DecimalField("Maximum years of Experience",validators=[DataRequired(NumberRange(min=0,max=99,message="Max experience needs be in range of 0 to 99"))])
+    b=[]
+    a=[]
+    for i in range(1,101):
+        a.append(i)
+
+    min_yrs_of_exp = SelectField("Minimum years of Experience",choices=a,default=0,validators=[DataRequired()])
+    max_yrs_of_exp = SelectField("Maximum years of Experience",choices=a,default=0,validators=[DataRequired(NumberRange(min=0,max=99,message="Min experience needs be in range of 0 to 99"))])
     
     primary_skills = TextAreaField('Primary Skills', validators=[DataRequired('Enter mandatory or good to have skillsets.')])
     secondary_skills = TextAreaField('Secondary Skills', validators=[DataRequired('Enter mandatory or good to have skillsets.')])
